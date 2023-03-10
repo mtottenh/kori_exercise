834737b74091273d563e02d5d737413d02d983ce
Max Tottenham <mtottenh@akamai.com>
2023-03-10 16:45:34 +0000

    Working solution.

--- validator/validator.py
+++ validator/validator.py
@@ -1,14 +1,49 @@
 from sys import stdin
 
+""""
+Things you might need:
+1. A list! To hold things. In python, we can use a list like list:
 
+# creating a new list:
+my_list = []
+
+# Adding an element to the list:
+my_list.append('a')
+
+# Print out what is in the list
+print(my_list)
+
+# Remove an element from the end of the list
+element = my_list.pop()
+"""
 
 def validate_parens(input: str) -> bool:
     '''
     Input: String to validate
     Output: True/False
     '''
-
-    return True
+    stack = []
+    for character in input:
+            match character:
+                case '{':
+                    stack.append(character)
+                case '[':
+                    stack.append(character)
+                case '(':
+                    stack.append(character)
+                case '}':
+                    top = stack.pop()
+                    if top != '{':
+                        return False
+                case ']':
+                    top = stack.pop()
+                    if top != '[':
+                        return False
+                case ')':
+                    top = stack.pop()
+                    if top != '(':
+                        return False
+    return len(stack) == 0
 
 if __name__ == "__main__":
     for line in stdin:
